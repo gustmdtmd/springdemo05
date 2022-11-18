5b044dba032f3e27839f9cce7b7f4746f61bf0e5
hs <kaos5930@gmail.com>
2022-11-18 21:01:29 +0900

    20221118 | update

---
+++ spring_05_jdbc/src/main/java/part01_mybatis/MemDaoImp.java
@@ -0,0 +1,59 @@
+package part01_mybatis;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemDaoImp implements MemDAO{
+	
+	//sqlSession로 생성된 Bean은 Autowired가 된다.
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	public MemDaoImp() {
+
+	}
+	
+	public void setSqlSession(SqlSessionTemplate sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+
+	@Override
+	public List<MemDTO> list() {
+		return sqlSession.selectList("mem.all");
+	}
+
+	@Override
+	public void insertMethod(MemDTO dto) {
+		sqlSession.insert("mem.ins",dto);
+	}
+
+	@Override
+	public MemDTO updateMethod(int num) {
+		return null;
+	}
+
+	@Override
+	public void updateMethod(MemDTO dto) {
+		sqlSession.update("mem.upt", dto);		
+	}
+
+	@Override
+	public void deleteMethod(int num) {
+		sqlSession.delete("mem.del", num);
+	}
+
+	@Override
+	public MemDTO one(int num) {
+		return sqlSession.selectOne("mem.one", num);
+	}
+
+	@Override
+	public int countMethod() {
+
+		return 0;
+	}
+}
